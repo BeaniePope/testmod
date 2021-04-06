267bfab83688cc3c71954aa5287122570940fcb2
BeaniePope <Swifter11110@gmail.com>
2021-04-06 14:48:57 -0400

    Initial Commit

---
+++ src/main/java/com/github/beaniepope/testmod/TestMod.java
@@ -0,0 +1,5 @@
+package com.github.beaniepope.testmod;
+
+public class TestMod {
+
+}
---
+++ src/main/java/com/github/beaniepope/testmod/client/TestModClient.java
@@ -0,0 +1,14 @@
+package com.github.beaniepope.testmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+
+    }
+}
